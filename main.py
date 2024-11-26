4a60e4ee29c1e0d656b3b35a68b60c64f77557df
Pasindu Yapa <lolmixage03@gmail.com>
2024-11-26 16:26:28 +0530

    Finished Modules and Packages

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import my_module;
+import my_module
+from my_module import add;
+import math;
+import base64 as b64;
 
-print(my_module.add(1,2))
+print(add(1,2))
+
+print(math.pi)
+
+print(math.factorial(4))
--- my_module.py
+++ my_module.py
@@ -1,3 +1,9 @@
 def add(a, b):
     result = a + b
     return result
+
+if __name__ == "__main__":
+    print("this code runs when the code is executed directly")
+    print(add(3,4))
+
+    
